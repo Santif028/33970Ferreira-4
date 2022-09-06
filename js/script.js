9fc39b060bcd64cce8cc02dc86f80f6abde0ceb3
Santiago Ferreira <santif028@gmail.com>
2022-09-05 21:45:06 -0300

    agrego json y cambios en js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,15 @@
-class Repuesto {
-  constructor(id, marca, nombre, precio, stock) {
-    this.id = id
-    this.marca = marca
-    this.nombre = nombre
-    this.precio = precio
-    this.stock = stock
-  }
+const divRepuestos = document.getElementById("divRepuestos")
+const botonTotal = document.getElementById("botonTotal")
+
+const traerRepuestos = async () => {
+  const response = await fetch('./js/json/repuestos.json')
+  const repuestos = await response.json()
+  return repuestos
 }
 
-const repuesto1 = new Repuesto("1", "Honda", "Carburador", 3500, 30)
-const repuesto2 = new Repuesto("2", "Yamaha", "Bomba de aceite", 3200, 23)
-const repuesto3 = new Repuesto("3", "Kawasaki", "Cigueñal", 5000, 20)
-const repuesto4 = new Repuesto("4", "Suzuki", "Arbol de levas", 2200, 15)
-const repuesto5 = new Repuesto("5", "BMW", "Kit de transimision", 1300, 13)
-
-const repuestos = [repuesto1, repuesto2, repuesto3, repuesto4, repuesto5]
-
-const divRepuestos = document.getElementById("divRepuestos")
-
-repuestos.forEach((repuestoArray) => {
-  divRepuestos.innerHTML += `
+function mostrarRepuestos(arrayRepuestos) {
+  arrayRepuestos.forEach((repuestoArray) => {
+    divRepuestos.innerHTML += `
     <div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">${repuestoArray.nombre}</h5>
@@ -30,28 +20,32 @@ repuestos.forEach((repuestoArray) => {
     </div>
   </div>
     `
+  })
+}
+
+traerRepuestos().then(repuestos => {
+  mostrarRepuestos(repuestos)
 })
 
 const carrito = []
-
 const botones = document.querySelectorAll(".boton-aniadir")
 
 botones.forEach((boton) => {
   boton.addEventListener("click", () => {
     const id = boton.dataset.id
-    const repuesto = repuestos.find((repuesto) => repuesto.id === id)
+    const repuesto = repuestoArray.find((repuesto) => repuesto.id === id)
     carrito.push(repuesto)
     console.log('carrito:', carrito);
   })
 })
 
 
-const botonTotal = document.getElementById("botonTotal")
+
 
 let total = 0
 const obtenerTotal = () => {
   carrito.forEach((repuestoArray) => {
-      total += repuestoArray.precio
+    total += repuestoArray.precio
   })
   return total
 }
